0c5ee59c2367c4ab6645c7242c5da1c077ff8ce7
Lily Sindayikengera <lily.sindayikengera@strathmore.edu>
2020-03-09 16:26:17 +0300

    beautiful

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Login Page</title>
+</head>
+<body>
+    <div class="container">
+
+    <form action="" method="">
+    Username:
+    <input type="text" name="username" placeholder=" Enter username..."> </br> </br>
+    Password:
+    <input type="password" name="password" placeholder=" Enter password..."></br>
+    </br>
+    <button type="submit" name="login"> LOGIN </button>
+    
+    
+    </form>
+
+    </div>
+</body>
+</html>
